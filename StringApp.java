a971af60b1bb84dc5f5c767117aa38bb1cb7bc23
leemartim <94498992+leemartim@users.noreply.github.com>
2022-04-07 02:01:19 +0900

    String 추가
    백슬래쉬 / String / Character / New Line

---
+++ StringApp.java
@@ -0,0 +1,19 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World"); // String
+		System.out.println('H'); // Java에서는 Character
+		System.out.println("H"); // 가능 
+		// Character 한글자를 표현하는 데이터 타입 
+		// String character가 모여진 묶음의 문자
+		
+		System.out.println("hello "
+				+ "world\n");
+		System.out.println("hello \nworld"); //new line
+		System.out.println("Hello \"world\""); // Hello "world"
+		// \(백스페이스) 의뒤에 내용은 escape 함 
+		
+	}
+
+}
