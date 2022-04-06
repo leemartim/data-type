5e4a03fecd969baf83800e79f0a04f82a58d8c61
leemartim <94498992+leemartim@users.noreply.github.com>
2022-04-07 01:02:55 +0900

    Add files via upload
    test

---
+++ datatype.java
@@ -0,0 +1,18 @@
+public class datatype {
+	public static void main(String[] args) {
+		System.out.println(6); // number
+		System.out.println("six"); // string (문자열)
+		
+		System.out.println("6"); // string 6
+		
+		System.out.println( 6 + 6 ); // 12
+		System.out.println( "6" + "6"); // 66
+		
+		System.out.println(6 * 6); // 36
+	//	System.out.println("6" * "6"); // string끼리는 곱할수없기에 error
+		
+		System.out.println("1111".length()); // 4
+	//  System.out.println(1111.length()); // 숫자의 길이를 알려주는 것은 없기에 error
+		
+	}
+}
