27f7cce883b144e844405e0d8920640c26b7ab22
leemartim <94498992+leemartim@users.noreply.github.com>
2022-04-07 12:57:32 +0900

    StringOperation
    length / replace

---
+++ StirngOperation.java
@@ -0,0 +1,10 @@
+
+public class StirngOperation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World".length()); // 11
+		System.out.println("Hello, [name]... bye.".replace("[name]", "Sanghoon"));
+	}
+
+}
