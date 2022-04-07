1cf2eaab2cc118a64f4865ca2837f58d8424a8d3
leemartim <94498992+leemartim@users.noreply.github.com>
2022-04-07 14:13:00 +0900

    Variable 추가
    integer / real number / string

---
+++ Variable.java
@@ -0,0 +1,16 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int a = 1; // number -> integer정수  ... -2,-1,0,1,2,....
+		System.out.println(a);
+		
+		double b = 1.1; // real number실수 ... -2.0, -1.1, 0, 1, 1.1, 2 .. 소수를 포함한 모든 숫자 
+		System.out.println(b);
+		
+		String c = "Hello World"; // String문자열  ... 
+		System.out.println(c); 
+	}
+
+}
+ 
